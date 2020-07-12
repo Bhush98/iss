c0c676c822cc0b3d316bbc622a4950ff244f5b90
Bhushan Rajput <bhushanrajput2298@gmail.com>
2020-07-12 14:00:58 +0530

    Updates

---
+++ precache-manifest.f97319da165b70f79e4cfbe21241edf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dae65daf0bb5a071073551dced0467",
+    "url": "/iss/index.html"
+  },
+  {
+    "revision": "5f389bc6f1e77a43bf0e",
+    "url": "/iss/static/css/main.e5894e63.chunk.css"
+  },
+  {
+    "revision": "579830f92b04238ff56c",
+    "url": "/iss/static/js/2.e310ce69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/iss/static/js/2.e310ce69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f389bc6f1e77a43bf0e",
+    "url": "/iss/static/js/main.196417cc.chunk.js"
+  },
+  {
+    "revision": "3f24876709912228b03b",
+    "url": "/iss/static/js/runtime-main.78c576fc.js"
+  },
+  {
+    "revision": "a7e0fc8b1ef6e910b5334e5c21fddc04",
+    "url": "/iss/static/media/251640.a7e0fc8b.jpg"
+  },
+  {
+    "revision": "776e39a549f2768e86129c2286e6f32f",
+    "url": "/iss/static/media/astro.776e39a5.png"
+  }
+]);
